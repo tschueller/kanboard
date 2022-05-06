1a9eda6bae125e0e887183a989fdca6029c17843
Thorsten Schüller <thorsten-nl@schueller.me>
2022-05-06 17:13:46 +0200

    hide unnecessary view buttons

--- app/Template/project_header/views.php
+++ app/Template/project_header/views.php
@@ -1,22 +1,22 @@
 <ul class="views">
-    
+<!-- hide by TSC, 06.05.2022: not necessary for my personal Kanban board
     <?= $this->hook->render('template:project-header:view-switcher-before-project-overview', array('project' => $project, 'filters' => $filters)) ?>
 
     <li <?= $this->app->checkMenuSelection('ProjectOverviewController') ?>>
         <?= $this->url->icon('eye', t('Overview'), 'ProjectOverviewController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-overview', t('Keyboard shortcut: "%s"', 'v o')) ?>
     </li>
-
+-->
     <?= $this->hook->render('template:project-header:view-switcher-before-board-view', array('project' => $project, 'filters' => $filters)) ?>
 
     <li <?= $this->app->checkMenuSelection('BoardViewController') ?>>
         <?= $this->url->icon('th', t('Board'), 'BoardViewController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-board', t('Keyboard shortcut: "%s"', 'v b')) ?>
     </li>
-
+<!-- hide by TSC, 06.05.2022: not necessary for my personal Kanban board
     <?= $this->hook->render('template:project-header:view-switcher-before-task-list', array('project' => $project, 'filters' => $filters)) ?>
 
     <li <?= $this->app->checkMenuSelection('TaskListController') ?>>
         <?= $this->url->icon('list', t('List'), 'TaskListController', 'show', array('project_id' => $project['id'], 'search' => $filters['search']), false, 'view-listing', t('Keyboard shortcut: "%s"', 'v l')) ?>
     </li>
-
+-->
     <?= $this->hook->render('template:project-header:view-switcher', array('project' => $project, 'filters' => $filters)) ?>
 </ul>
